4a3b69fede0a709e418eed6eac2518655d00f62b
Abdelrahman-Fawzy <Abdelrahmanawzy98@gmail.com>
2023-07-11 17:09:19 +0300

    First Version

---
+++ dist/scripts/plugin.js
@@ -0,0 +1,48 @@
+filterSelection("all", '')
+function filterSelection(c, event) {
+    var x, i;
+    x = document.getElementsByClassName("task");
+    if (c == "all") c = "";
+    // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+    for (i = 0; i < x.length; i++) {
+    x[i].parentElement.classList.remove("showElement")
+        if (x[i].className.indexOf(c) > -1) x[i].parentElement.classList.add("showElement");
+    }
+    // Add active class to the current control button (highlight it)
+    var btnContainer = document.getElementById("filters");
+    var btns = btnContainer.getElementsByClassName("filterBtn");
+    for (var i = 0; i < btns.length; i++) {
+        if (event != '')
+            btns[i].classList.remove("active")
+    }
+    event.target.classList.add('active')
+}
+
+function respondOnMessage() {
+    var button = document.getElementById("responde_on_message")
+    var responde_box = document.getElementById("message-resposnse")
+
+    responde_box.style.display = "block"
+    button.children[1].innerHTML = "ارسل رسالتك"
+}
+
+function deleteMessage() {
+    var button = document.getElementById("responde_on_message")
+    var responde_box = document.getElementById("message-resposnse")
+
+    responde_box.children[0].children[0].value = ""
+    responde_box.style.display = "none"
+    button.children[1].innerHTML = "الرد على الرسالة"
+}
+
+function saveMessage() {
+    var form_content = document.getElementById("form_content")
+    var success_content = document.getElementById("success_content")
+
+    form_content.style.display = "none"
+    success_content.style.display = "flex"
+
+    $( "#form_content" ).submit(function( event ) {
+        event.preventDefault();
+    });
+}
